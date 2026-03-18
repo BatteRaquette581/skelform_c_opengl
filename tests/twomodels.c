a3a6cfe0b64555457b25bb683c8ec22155fd17d6
BuildTools <unconfigured@null.spigotmc.org>
2026-03-18 19:24:27 +0100

    _strdup pointer from integer without cast fix

--- tests/twomodels.c
+++ tests/twomodels.c
@@ -80,8 +80,8 @@ int main()
     skf_bool reverse = skf_false;
     skf_bool wireframe = skf_false;
     uint64_t start = skf_opengl_get_epoch_ms();
-    glfwSetWindowTitle(window, strcat(strcat(_strdup(file1->armature.animations.elements[0].name), " | "),
-        file2->armature.animations.elements[0].name));
+    glfwSetWindowTitle(window, strcat(strcat((char*) _strdup(file1->armature.animations.elements[0].name),
+        " | "), file2->armature.animations.elements[0].name));
 
     while (!glfwWindowShouldClose(window)) {
         double t1 = glfwGetTime();
@@ -97,8 +97,8 @@ int main()
                 break;
             if (glfwGetKey(window, GLFW_KEY_0 + i) == GLFW_PRESS) {
                 animations2.elements[0] = file2->armature.animations.elements[i];
-                glfwSetWindowTitle(window, strcat(strcat(_strdup(file1->armature.animations.elements[i].name), " | "),
-                    file2->armature.animations.elements[i].name));
+                glfwSetWindowTitle(window, strcat(strcat((char*) _strdup(file1->armature.animations.elements[i].name),
+                    " | "), file2->armature.animations.elements[i].name));
             }
         }
         if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS)
